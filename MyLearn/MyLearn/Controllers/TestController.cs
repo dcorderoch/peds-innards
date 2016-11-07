cc8dad53bbf8d4eaa312186efb0391289b6db52c
El David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-07 17:21:51 -0600

    add MyLearn Visual Studio Solution

---
+++ MyLearn/MyLearn/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace MyLearn.Controllers
+{
+    public class TestController : ApiController
+    {
+        /// <summary>
+        /// This Method returns all information of all Products in the Database
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public JsonResult<List<string>> GetAll()
+        {
+            List<string> retVal = new List<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                var crap = new string(string.Format("haha{0}", i.ToString()).ToCharArray());
+                retVal.Add(crap);
+            }
+
+            return Json(retVal);
+        }
+    }
+}
---
+++ MyLearn/MyLearn/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyLearn.WebApiApplication" Language="C#" %>
