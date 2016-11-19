dbdf34870d83de50d44772cd19c497b62ec97b30
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-18 21:14:32 -0600

    add Register API methods and URIs

--- MyLearn/MyLearn/Models/RegisterEstudianteInfo.cs
+++ MyLearn/MyLearn/Models/RegisterEstudianteInfo.cs
@@ -1,18 +1,22 @@
-﻿namespace MyLearn.Models
+﻿using System.Collections.Generic;
+
+namespace MyLearn.Models
 {
-    public class InfoProfesor
+    public class RegisterEstudianteInfo
     {
         public string NombreContacto { get; set; }
         public string ApellidoContacto { get; set; }
         public string Ubicacion { get; set; }
         public string Email { get; set; }
         public string Telefono { get; set; }
-        public string Fecha_Registro { get; set; }
         public string Password { get; set; }
         public string TipoRepositorioArchivos { get; set; }
-        public string Foto { get; set; } // Opcional
-        public string IdProfesor { get; set; }
+        public string Foto { get; set; }
+        public string Carnet { get; set; }
         public string Universidad { get; set; }
-        public string HorarioAtencion { get; set; }
+        public string EnlaceRepositorioCodigo { get; set; }
+        public string EnlaceACurriculum { get; set; }
+        public List<string> Idiomas { get; set; }
+        public List<string> Tecnologias { get; set; }
     }
 }
