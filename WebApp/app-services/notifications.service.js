edb93d6087cb4fa6e2f4e863ef68b3701f4e0325
Majesco <nicolas.j2007@gmail.com>
2016-11-19 22:25:24 -0600

    add alignment with api (notifications)

---
+++ WebApp/app-services/notifications.service.js
@@ -0,0 +1,39 @@
+(function () {
+
+    angular
+        .module('app')          
+        .factory('NotificationsService', NotificationsService);
+
+    NotificationsService.$inject = ['$http','$rootScope'];
+    function NotificationsService($http, $rootScope) {
+
+        var service = {};
+
+        service.GetNotifications = GetNotifications;
+
+        return service;
+  
+        function GetNotifications(Carnet) {
+            var response=$http({
+                method:"post",
+                url:$rootScope.url+"Api/Notifications/GetByStudent",
+                data: {"Carnet":Carnet}
+            });
+            return response;    
+        }
+
+       
+        // private functions
+
+        function handleSuccess(res) {
+            return res.data;
+        }
+
+        function handleError(error) {
+            return function () {
+                return { success: false, message: error };
+            };
+        }
+    }
+
+})();
--- WebApp/notifications/notifications.controller.js
+++ WebApp/notifications/notifications.controller.js
@@ -5,9 +5,34 @@
         .module('app')
         .controller('NotificationsController', NotificationsController);
 
-    NotificationsController.$inject = ['$location',  'FlashService'];
-    function NotificationsController($location,  FlashService) {
+    NotificationsController.$inject = ['$location',  'FlashService', '$rootScope', 'NotificationsService'];
+    function NotificationsController($location,  FlashService, $rootScope, NotificationsService) {
         var vm = this;
+
+        vm.notifications=[];
+        initController();
+
+        function initController(){
+            
+            vm.notifications.push("algo perdido");
+            vm.notifications.push("algo ganado");
+            
+            getNotifications();
+            console.log(vm.notifications);
+        }
+
+        var Carnet = $rootScope.userData.Carnet;
+
+        function getNotifications(){
+            NotificationsService.GetNotifications( Carnet )
+                .then( function(response){
+                if (response.suceess){
+                    vm.notifications = response.data.Notifications;
+                }
+            }, function(response){
+                console.log("no se pudo obtener")
+            });       
+        }
 
 
     }
