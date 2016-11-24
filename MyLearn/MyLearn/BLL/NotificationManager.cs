9986fe5da202a75ceccd9204d2b4f7ba6839d08c
Kevin <kevgiso@hotmail.com>
2016-11-24 16:10:02 -0600

    Add notification manager

---
+++ MyLearn/MyLearn/BLL/NotificationManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyLearn.BLL
+{
+    public class NotificationManager
+    {
+        public List<string> GetStudentNotifications(string studentId)
+        {
+            List<string> notificationList = new List<string>();
+            //Retrieve notifications from DB
+            //subject to change
+
+
+            return notificationList;
+
+        }
+    
+    }
+}
