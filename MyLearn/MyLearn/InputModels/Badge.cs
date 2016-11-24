19cd5788848c0ebfcf9a9a61c571e08e3247e4da
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-24 17:50:53 -0600

    add newcourse model

---
+++ MyLearn/MyLearn/InputModels/Badge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLearn.InputModels
+{
+    public class Badge
+    {
+        public string BadgeDescription { get; set; }
+        public int Value { get; set; }
+        public int Alardeado { get; set; }
+    }
+}
---
+++ MyLearn/MyLearn/InputModels/NewCourse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLearn.InputModels
+{
+    public class NewCourse
+    {
+        public string CourseName { get; set; }
+        public string CourseDescription { get; set; }
+        public string ProfUserId { get; set; }
+        public string UniversityId { get; set; }
+        public int Group { get; set; }
+        public int MinGrade { get; set; }
+        public List<Badge> Badges { get; set; }
+    }
+}
