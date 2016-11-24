9a4468bc8f86b7c4c3c8287ce3bd26a1ddf93677
Kevin <kevgiso@hotmail.com>
2016-11-24 16:32:50 -0600

    Add Search Manager.

---
+++ MyLearn/MyLearn/BLL/SearchManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLearn.BLL
+{
+    public class SearchManager
+    {
+        public List<TopStudent> GetTopStudents(int numberOfStudents)
+        {
+            
+        }
+        
+        public List<TopStudent> GetTopStudentsByCriteria(int numberOfStudents, int courseAvgWeight, 
+            int courseSuccessRateWeight, int projectAvgWeight, int projectSuccessRateWeight)
+        {
+            int i = 0;
+            while ()
+            {
+                
+            }
+        }
+    }
+}
