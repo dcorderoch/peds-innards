c8fb7922aee7a71144cdf6f1df6bc5f50015b212
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-24 17:36:38 -0600

    add getcourseasprofessor model

---
+++ MyLearn/MyLearn/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLearn.Models
+{
+    public class Course
+    {
+        public string CourseName { get; set; }
+        public string UniversityId { get; set; }
+        public int MinGrade { get; set; }
+        public string CourseId { get; set; }
+        public string CourseDescription { get; set; }
+        public int Group { get; set; }
+        public List<StudentInCourse> Students { get; set; }
+    }
+}
---
+++ MyLearn/MyLearn/Models/StudentInCourse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLearn.Models
+{
+    public class StudentInCourse
+    {
+        public string Nombre { get; set; }
+        public string Carnet { get; set; }
+    }
+}
