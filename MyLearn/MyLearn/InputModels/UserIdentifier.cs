42208c040d83cc9f5e0fb3da7039ad4acc2a9b38
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-24 08:29:36 -0600

    add missing joboffer models

---
+++ MyLearn/MyLearn/InputModels/UserIdentifier.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLearn.InputModels
+{
+    public class UserIdentifier
+    {
+        public string UserId { get; set; }
+    }
+}
