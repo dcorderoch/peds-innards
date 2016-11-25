18c04e343b1d9f9efca33a6dbec862ba837b9e7c
Manuel Zumbado <Manuel Zumbado>
2016-11-24 23:13:13 -0600

    Add technology model

---
+++ MyLearn/MyLearnDAL/Models/Technology.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyLearnDAL.Models
+{
+    [Table("Tecnology")]
+    public class Technology
+    {
+        public Guid TecnologyId { get; set; }
+        public string Name { get; set; }
+    }
+}
