e3c627aaf0506c4db2d8f6ebf08a0763befb125a
Manuel Zumbado <Manuel Zumbado>
2016-11-24 23:13:37 -0600

    Add studenttechnology model

---
+++ MyLearn/MyLearnDAL/Models/StudentTechnology.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace MyLearnDAL.Models
+{
+    [Table("StudentTechnology")]
+    public class StudentTechnology
+    {
+        [Key]
+        [ForeignKey("Technology")]
+        public Guid TechnologyId { get; set; }
+        [Key]
+        [ForeignKey("Student")]
+        public Guid UserId { get; set; }
+
+        public virtual Technology Technology { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
