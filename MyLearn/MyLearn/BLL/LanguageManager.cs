104e5730b83a77e2a37fca22afbd6a9643174833
Kevin <kevgiso@hotmail.com>
2016-11-25 02:55:42 -0600

    Add language manager skeleton

---
+++ MyLearn/MyLearn/BLL/LanguageManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyLearn.Models;
+
+namespace MyLearn.BLL
+{
+    public class LanguageManager
+    {
+        public List<Language> GetAllLanguages()
+        {
+            List<Language> allLanguages = new List<Language>();
+
+            //code goes here
+
+            return allLanguages;
+        }
+    }
+}
---
+++ MyLearn/MyLearn/Models/Language.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLearn.Models
+{
+    public class Language
+    {
+        public string LanguageName { get; set; }
+        public string LanguageId { get; set; }
+    }
+}
