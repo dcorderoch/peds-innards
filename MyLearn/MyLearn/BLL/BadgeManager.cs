59f398be418845fb3e37765967dac98db9f95a71
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-25 16:53:47 -0600

    add badge controller and manager

---
+++ MyLearn/MyLearn/BLL/BadgeManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyLearn.InputModels;
+using MyLearn.Models;
+
+namespace MyLearn.BLL
+{
+    public class BadgeManager
+    {
+        public ReturnCode GiveBadge(NewBadge newBadge)
+        {
+            var retVal = new ReturnCode();
+            // SUBJECT TO CHANGE
+            return retVal;
+        }
+    }
+}
---
+++ MyLearn/MyLearn/Controllers/BadgeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using MyLearn.Models;
+using MyLearn.InputModels;
+using MyLearn.BLL;
+
+namespace MyLearn.Controllers
+{
+    public class BadgeController : ApiController
+    {
+        [HttpPost]
+        public JsonResult<ReturnCode> Give(NewBadge newBadge)
+        {
+            var status = new BadgeManager();
+            var retVal = status.GiveBadge(newBadge);
+            return Json(retVal);
+        }
+    }
+}
