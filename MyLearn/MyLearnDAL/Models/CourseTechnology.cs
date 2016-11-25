4810520126679091936b8dc776224370f98c26ab
Manuel Zumbado <Manuel Zumbado>
2016-11-24 23:18:29 -0600

    Add CourseTechnology model

---
+++ MyLearn/MyLearnDAL/Models/CourseTechnology.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyLearnDAL.Models
+{
+    [Table("CourseTechnology")]
+    public class CourseTechnology
+    {
+        [Key]
+        [ForeignKey("Technology")]
+        public Guid TechnologyId { get; set; }
+        [Key]
+        [ForeignKey("Course")]
+        public Guid CourseId { get; set; }
+
+        public virtual Technology Technology { get; set; }
+        public virtual Course Course { get; set; }
+    }
+}
