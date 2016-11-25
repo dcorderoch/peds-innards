b25155e61cd9bd3cef07728684f8b49db3c7880c
Manuel Zumbado <Manuel Zumbado>
2016-11-24 23:17:21 -0600

    Add jobOfferTechnology model

---
+++ MyLearn/MyLearnDAL/Models/JobOfferTechnology.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyLearnDAL.Models
+{
+    [Table("JobOfferTechnology")]
+    public class JobOfferTechnology
+    {
+        [Key]
+        [ForeignKey("JobOffer")]
+        public Guid JobOfferId { get; set; }
+        [Key]
+        [ForeignKey("Technology")]
+        public Guid TechnologyId { get; set; }
+
+        public virtual JobOffer JobOffer { get; set; }
+        public virtual Technology Technology { get; set; }
+    }
+}
