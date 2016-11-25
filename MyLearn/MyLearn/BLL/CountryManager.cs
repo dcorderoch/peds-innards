be8a606e006a262c31ecbcf8d85096f8c10124ae
Kevin <kevgiso@hotmail.com>
2016-11-25 01:49:43 -0600

    Complete Country Manager skeleton.

---
+++ MyLearn/MyLearn/BLL/CountryManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyLearn.Models;
+
+namespace MyLearn.BLL
+{
+    public class CountryManager
+    {
+        public List<Country> GetAllCountries()
+        {
+            List<Country> countries = new List<Country>();
+
+            return countries;
+        }
+    }
+}
---
+++ MyLearn/MyLearn/Models/Country.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLearn.Models
+{
+    public class Country
+    {
+        public string CountryName { get; set; }
+        public string CountryId { get; set; }
+    }
+}
