88cf521c41f79850bda26a5bebcd67c156b9efd0
Kevin <kevgiso@hotmail.com>
2016-11-25 03:26:36 -0600

    Add University Manager skeleton.

---
+++ MyLearn/MyLearn/BLL/UniversityManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyLearn.Models;
+
+namespace MyLearn.BLL
+{
+    public class UniversityManager
+    {
+        public List<University> GetAllUniversities()
+        {
+            List<University> allUniversities = new List<University>();
+
+            //code goes here
+
+            return allUniversities;
+        }
+    }
+}
---
+++ MyLearn/MyLearn/Models/University.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLearn.Models
+{
+    public class University
+    {
+        public string UniversityName { get; set; }
+        public string UniversityId { get; set; }
+    }
+}
