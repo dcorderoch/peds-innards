61e67672d53568f6e1900bfdb30b8e043e09e9fe
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-25 16:56:07 -0600

    add account controller

---
+++ MyLearn/MyLearn/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using MyLearn.Models;
+using MyLearn.InputModels;
+using MyLearn.BLL;
+
+namespace MyLearn.Controllers
+{
+    public class AccountController : ApiController
+    {
+        [HttpPost]
+        public JsonResult<ReturnCode> Disable(UserIdentifier userId)
+        {
+            var status = new AccountManager();
+            var retVal = status.DisableAccount(userId.UserId);
+            return Json(retVal);
+        }
+    }
+}
