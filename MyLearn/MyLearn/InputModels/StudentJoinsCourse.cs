0c9b4ee5bf9827f07a0e89aa9a2395b2cdccf404
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-25 16:58:57 -0600

    add studentjoinscourse model

---
+++ MyLearn/MyLearn/InputModels/StudentJoinsCourse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLearn.InputModels
+{
+    public class StudentJoinsCourse
+    {
+        public string CourseId { get; set; }
+        public string StudentUserId { get; set; }
+    }
+}
