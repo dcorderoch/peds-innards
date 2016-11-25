f2adf4c47e4872c80cf45ae9297cb0b094d3ce49
Manuel Zumbado <Manuel Zumbado>
2016-11-24 23:16:17 -0600

    Add notification model

---
+++ MyLearn/MyLearnDAL/Models/Notification.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace MyLearnDAL.Models
+{
+    [Table("Notification")]
+    public class Notification
+    {
+        [Key]
+        public Guid NotificationId { get; set; }
+        [Required]
+        [ForeignKey("Student")]
+        public Guid UserId { get; set; }
+        [Required]
+        public string Message { get; set; }
+        [Required]
+        public int State { get; set; }
+
+        public virtual Student Student { get; set; }
+    }
+}
