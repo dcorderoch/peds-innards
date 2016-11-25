b9bebc38d72e4485c6907e7914a4b1d7e1987df7
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-25 13:35:30 -0600

    add finish freelance to API

---
+++ MyLearn/MyLearn/InputModels/CloseJobOffer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLearn.InputModels
+{
+    public class CloseJobOffer
+    {
+        public string JobOfferId { get; set; }
+        public string State { get; set; }
+        public string StateDescription { get; set; }
+    }
+}
