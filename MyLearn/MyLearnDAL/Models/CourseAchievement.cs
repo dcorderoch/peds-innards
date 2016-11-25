0010b89a0f10a9cad5858f14ed4d71814ae8623b
Manuel Zumbado <Manuel Zumbado>
2016-11-24 23:18:47 -0600

    Add CourseAchievement model

---
+++ MyLearn/MyLearnDAL/Models/CourseAchievement.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyLearnDAL.Models
+{
+    [Table("CourseAchievement")]
+    public class CourseAchievement
+    {
+        [Key]
+        [ForeignKey("Achievement")]
+        public Guid AchievementId { get; set; }
+        [Key]
+        [ForeignKey("Course")]
+        public Guid CourseId { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Achievement Achievement { get; set; }
+    }
+}
