fc3de729630eae2a699d0a784e4e67de974eaf0f
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-24 18:20:50 -0600

    add newjoboffer input model

---
+++ MyLearn/MyLearn/InputModels/NewJobOffer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLearn.InputModels
+{
+    public class NewJobOffer
+    {
+        public string JobOffer { get; set; }
+        public List<string> Technologies { get; set; }
+        public string Location { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string Description { get; set; }
+        public int Budget { get; set; }
+    }
+}
