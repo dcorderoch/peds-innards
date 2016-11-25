8fd4fd8b5f2b6b6c94723c37a4ebd302df305208
Kevin <kevgiso@hotmail.com>
2016-11-24 21:41:20 -0600

    Add Job Manager

---
+++ MyLearn/MyLearn/BLL/JobManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyLearn.InputModels;
+using MyLearn.Models;
+
+namespace MyLearn.BLL
+{
+    public class JobManager
+    {
+        public ReturnCode CreateJobOffer(NewJobOffer newOffer)
+        {
+            ReturnCode success = new ReturnCode();
+            
+            //Create new offer
+            //Add new course to DB
+            /*dbobject.Add(newOffer.JobOffer);
+            dbobject.Add(newOffer.Technologies);
+            dbobject.Add(newOffer.Location);
+            dbobject.Add(newOffer.StartDate);
+            dbobject.Add(newOffer.EndDate);
+            dbobject.Add(newOffer.Description);
+            dbobject.Add(newOffer.Budget);*/
+
+            success.StatusCode = 1;
+            return success;
+        }
+
+        public AllJobOffers GetJobOffersByEmployer(string employerId)
+        {
+            AllJobOffers jobOffers = new AllJobOffers();
+            //get all employer's offers
+            ActiveJobOffersList activeJobs = new ActiveJobOffersList();
+            FinishedJobOffersList finishedJobs = new FinishedJobOffersList();
+            //get from db
+            activeJobs = new ActiveJobOffersList();
+            finishedJobs = new FinishedCoursesList();
+
+            allCourses.ActiveCourses = activeCourses;
+            allCourses.FInishedCourses = finishedCourses;
+
+            return allCourses;
+        }
+    }
+}
---
+++ MyLearn/MyLearn/Models/AllJobOffers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLearn.Models
+{
+    public class AllJobOffers
+    {
+        public ActiveJobOffersList ActiveJobOffers { get; set; }
+        public FinishedJobOffersList FinishedJobOffers { get; set; }
+    }
+}
