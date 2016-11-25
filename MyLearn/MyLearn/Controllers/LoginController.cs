c7b20c34cda851f7fc3f81fc5e1b9111db391116
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-25 13:12:54 -0600

    add state and state desccription to job offers

--- MyLearn/MyLearn/Controllers/LoginController.cs
+++ MyLearn/MyLearn/Controllers/LoginController.cs
@@ -7,35 +7,35 @@ namespace MyLearn.Controllers
 {
     public class LoginController : ApiController
     {
-        //[RequireHttps]
+        
         [HttpPost]
         public JsonResult<UserCode> Login(LoginInfo userCredentials) {
             AccountManager LoginFromBLL = new AccountManager();
             var retVal = LoginFromBLL.GetUserTypeCode(userCredentials.UserName,userCredentials.Password);
             return Json(retVal);
         }
-        //[RequireHttps]
+        
         [HttpPost]
         public JsonResult<InfoEstudiante> LoginStudent(LoginInfo userCredentials) {
             AccountManager LoginFromBLL = new AccountManager();
             var retVal = LoginFromBLL.StudentLogin(userCredentials.UserName, userCredentials.Password);
             return Json(retVal);
         }
-        //[RequireHttps]
+        
         [HttpPost]
         public JsonResult<InfoProfesor> LoginProfessor(LoginInfo userCredentials) {
             AccountManager LoginFromBLL = new AccountManager();
             var retVal = LoginFromBLL.ProfessorLogin(userCredentials.UserName, userCredentials.Password);
             return Json(retVal);
         }
-        //[RequireHttps]
+        
         [HttpPost]
         public JsonResult<InfoEmpleador> LoginEmployer(LoginInfo userCredentials) {
             AccountManager LoginFromBLL = new AccountManager();
             var retVal = LoginFromBLL.EmployerLogin(userCredentials.UserName, userCredentials.Password);
             return Json(retVal);
         }
-        //[RequireHttps]
+        
         [HttpPost]
         public JsonResult<InfoAdmin> LoginAdmin(LoginInfo userCredentials) {
             AccountManager LoginFromBLL = new AccountManager();
