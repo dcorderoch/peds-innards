c432742ea10e03aae18dc1af07af92af774179cf
Manuel Zumbado <Manuel Zumbado>
2016-11-24 23:14:58 -0600

    Add projectTechnology model

---
+++ MyLearn/MyLearnDAL/Models/ProjectTechnology.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace MyLearnDAL.Models
+{
+    [Table("ProjectTechnology")]
+    class ProjectTechnology
+    {
+        [Key]
+        [ForeignKey("Project")]
+        public Guid ProjectId { get; set; }
+        [Key]
+        [ForeignKey("Technology")]
+        public Guid TechnologyId { get; set; }
+
+        public virtual Project Project { get; set; }
+        public virtual Technology Technology { get; set; }
+    }
+}
