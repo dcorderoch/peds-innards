323d038714ffc9541b68195f566d4fdef9f22232
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-24 19:43:08 -0600

    add missing all courses model

---
+++ MyLearn/MyLearn/Models/AllProfessorsCourses.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLearn.Models
+{
+    public class AllProfessorsCourses
+    {
+        public ActiveCoursesList ActiveCourses { get; set; }
+        public FinishedCoursesList FInishedCourses { get; set; }
+    }
+}
