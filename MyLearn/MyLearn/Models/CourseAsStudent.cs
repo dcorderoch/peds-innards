09ea673536d6f1a0a271e006c4995de86a57e42e
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-24 18:42:25 -0600

    add course as student model

---
+++ MyLearn/MyLearn/Models/CourseAsStudent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLearn.Models
+{
+    public class CourseAsStudent
+    {
+        public string CourseName { get; set; }
+        public string StudentUserId { get; set; }
+        public string ProfUserId { get; set; }
+        public string ProfessorName { get; set; }
+        public string UniversityId { get; set; }
+        public int Grade { get; set; }
+        public List<Badge> Badges { get; set; }
+        public string CourseId { get; set; }
+        public string CourseDescription { get; set; }
+        public int Group { get; set; }
+        public int CourseState { get; set; }
+    }
+}
