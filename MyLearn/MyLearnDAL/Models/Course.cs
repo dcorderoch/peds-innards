059f21c6e9a3b460365724b3dcce51a446212629
Manuel Zumbado <Manuel Zumbado>
2016-11-24 23:19:01 -0600

    Add Course model

---
+++ MyLearn/MyLearnDAL/Models/Course.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyLearnDAL.Models
+{
+    [Table("Course")]
+    public class Course
+    {
+        public Course()
+        {
+            Students = new List<Student>();
+            Technologies= new List<Technology>();
+            Achievements = new List<Achievement>();
+        }
+
+        [Key]
+        public Guid CourseId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int Group { get; set; }
+        [Required]
+        [ForeignKey("Professor")]
+        public Guid ProfessorId { get; set; }
+        [Required]
+        [ForeignKey("University")]
+        public Guid UniversityId { get; set; }
+        [Required]
+        public decimal MinScore { get; set; }
+        [Required]
+        public int IsActive { get; set; }
+
+        public virtual Professor Professor { get; set; }
+        public virtual University University { get; set; }
+        public virtual List<Student> Students { get; set; }
+        public virtual List<Technology> Technologies { get; set; }
+        public virtual List<Achievement> Achievements { get; set; }
+    }
+}
