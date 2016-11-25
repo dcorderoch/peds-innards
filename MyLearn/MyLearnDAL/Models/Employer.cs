0f7f9e192f8159c8801ac47176fb7e47a3678e19
Manuel Zumbado <Manuel Zumbado>
2016-11-24 23:18:08 -0600

    Add Employer model

---
+++ MyLearn/MyLearnDAL/Models/Employer.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace MyLearnDAL.Models
+{
+    [Table("Employer")]
+    public class Employer : User
+    {
+        public Employer()
+        {
+            JobOffers = new List<JobOffer>();
+        }
+
+        [Required]
+        [MaxLength(30)]
+        public string CompanyName { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string ContactName { get; set; }
+        [MaxLength(30)]
+        public string Website { get; set; }
+
+        public virtual List<JobOffer> JobOffers { get; set; }
+    }
+}
