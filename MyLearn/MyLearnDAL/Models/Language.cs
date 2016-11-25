6e3ce2520cadf3d27f302fdcbac064dce3d3a623
Manuel Zumbado <Manuel Zumbado>
2016-11-24 23:16:40 -0600

    Add language model

---
+++ MyLearn/MyLearnDAL/Models/Language.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace MyLearnDAL.Models
+{
+    [Table("Language")]
+    public class Language
+    {
+        [Key]
+        public int LenguageId { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
