4af0626d7ea8b3dde5768494d6ce5c40b529e3bc
Manuel Zumbado <Manuel Zumbado>
2016-11-24 23:23:49 -0600

    add achievement model

---
+++ MyLearn/MyLearnDAL/Models/Achievement.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyLearnDAL.Models
+{
+    [Table("Achievement")]
+    public class Achievement
+    {
+        [Key]
+        public Guid AchievementId {get; set; }
+        [Required]
+        [ForeignKey("Course")]
+        public Guid CourseId { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public int Score { get; set; }
+
+        public virtual Course Course { get; set; }
+    }
+}
