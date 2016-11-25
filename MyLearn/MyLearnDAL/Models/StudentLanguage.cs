661f62be8ad75d6d6f8f798b4a1af826668beb07
Manuel Zumbado <Manuel Zumbado>
2016-11-24 23:14:03 -0600

    Add studentlanguage model

---
+++ MyLearn/MyLearnDAL/Models/StudentLanguage.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyLearnDAL.Models
+{
+    [Table("StudentLanguage")]
+    public class StudentLanguage
+    {
+        [Key]
+        [ForeignKey("Student")]
+        public Guid UserId { get; set; }
+        [Key]
+        [ForeignKey("Language")]
+        public int LanguageId { get; set; }
+
+        public virtual Student Student { get; set; }
+        public virtual Language Language { get; set; }
+    }
+}
