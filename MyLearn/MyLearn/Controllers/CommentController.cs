a0bcaafbcee20c30ae54c52da4a6861dc98b17fe
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-25 16:57:03 -0600

    add comment controller

---
+++ MyLearn/MyLearn/Controllers/CommentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using MyLearn.BLL;
+using MyLearn.Models;
+using MyLearn.InputModels;
+
+namespace MyLearn.Controllers
+{
+    public class CommentController : ApiController
+    {
+        [HttpPost]
+        public JsonResult<List<Comment>> GetAll(CommentInfo commentInfo)
+        {
+            var cmtMngr = new CommentManager();
+            var retVal = cmtMngr.GetAllComments(commentInfo);
+            return Json(retVal);
+        }
+        [HttpPost]
+        public JsonResult<ReturnCode> Create(NewComment newComment)
+        {
+            var cmtMngr = new CommentManager();
+            var retVal = cmtMngr.CreateComment(newComment);
+            return Json(retVal);
+        }
+    }
+}
