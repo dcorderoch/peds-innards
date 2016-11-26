272c73f2350796b89450529a7193babaed253b9e
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-25 19:26:27 -0600

    add technology controller

---
+++ MyLearn/MyLearn/Controllers/TechnologyController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using MyLearn.BLL;
+using MyLearn.Models;
+using MyLearn.InputModels;
+
+namespace MyLearn.Controllers
+{
+    public class TechnologyController : ApiController
+    {
+        [HttpPost]
+        public JsonResult<List<Technology>> GetAll()
+        {
+            var techMngr = new TechnologyManager();
+            var retVal = techMngr.GetAllTechnologies();
+            return Json(retVal);
+        }
+        [HttpPost]
+        public JsonResult<ReturnCode> Create(NewTechnology newTech)
+        {
+            var techMngr = new TechnologyManager();
+            var retVal = techMngr.CreateTechnology(newTech.TechnologyName);
+            return Json(retVal);
+        }
+    }
+}
