d1eb3303a48ca0c394b0487d84b924ffa4723f6b
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-25 18:58:26 -0600

    add language controller

---
+++ MyLearn/MyLearn/Controllers/LanguageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using MyLearn.BLL;
+using MyLearn.Models;
+using MyLearn.InputModels;
+
+namespace MyLearn.Controllers
+{
+    public class LanguageController : ApiController
+    {
+        [HttpGet]
+        public JsonResult<List<Language>> GetAll()
+        {
+            var langMngr = new LanguageManager();
+            var retVal = langMngr.GetAllLanguages();
+            return Json(retVal);
+        }
+    }
+}
