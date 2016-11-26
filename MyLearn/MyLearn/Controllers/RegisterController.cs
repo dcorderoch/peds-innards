91839d780697ef71446879e5bd88597eaa2f25a1
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-25 18:44:20 -0600

    correct using directoives

--- MyLearn/MyLearn/Controllers/RegisterController.cs
+++ MyLearn/MyLearn/Controllers/RegisterController.cs
@@ -1,5 +1,11 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
 using System.Web.Http.Results;
+using MyLearn.BLL;
 using MyLearn.Models;
 using MyLearn.InputModels;
 
