6d533a30e1e069b8a3b6393a35e2aa55c7588f3e
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-25 19:05:56 -0600

    add notification controller and methods

---
+++ MyLearn/MyLearn/Controllers/NotificationController.cs
@@ -0,0 +1,19 @@
+﻿using MyLearn.BLL;
+using MyLearn.InputModels;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace MyLearn.Controllers
+{
+    public class NotificationController : ApiController
+    {
+        [HttpPost]
+        public JsonResult<List<string>> GetByStudent(StudentIdentifier studentID)
+        {
+            var notifMngr = new NotificationManager();
+            var retVal = notifMngr.GetStudentNotifications(studentID.StudentUserId);
+            return Json(retVal);
+        }
+    }
+}
---
+++ MyLearn/MyLearn/InputModels/StudentIdentifier.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLearn.InputModels
+{
+    public class StudentIdentifier
+    {
+        public string StudentUserId { get; set; }
+    }
+}
