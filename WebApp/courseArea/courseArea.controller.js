b47a33038fd68891bd06ffd4013b7340ff63938c
Majesco <nicolas.j2007@gmail.com>
2016-11-25 18:04:29 -0600

    coursearea

--- WebApp/courseArea/courseArea.controller.js
+++ WebApp/courseArea/courseArea.controller.js
@@ -5,25 +5,60 @@
         .module('app')
         .controller('CourseAreaController', CourseAreaController);
 
-    CourseAreaController.$inject = ['$location',  'FlashService', '$rootScope', 'CourseService', 'ProfileCourseService'];
-    function CourseAreaController($location,  FlashService, $rootScope, CourseService, ProfileCourseService) {
+    CourseAreaController.$inject = ['$location',  'FlashService', '$rootScope', 'CourseService', 'ProfileCourseService', 'RegService'];
+    function CourseAreaController($location,  FlashService, $rootScope, CourseService, ProfileCourseService, RegService) {
         var vm = this;
+
+        vm.suggestProject = suggestProject;
         
-        
-        
+        vm.technologies=[{ Technology:"Java", TechnologyId: "0"}, { Technology:"C++",TechnologyId: "1"}];
+        var Tecnologias = [];
         initController();
+        
         function initController(){
 
             vm.courseData =ProfileCourseService.GetCourseData();
             vm.courseData.Carnet = $rootScope.userData.Carnet;
             //            vm.courseData.status=true;
             vm.gradeWidth = {'width': vm.courseData.Grade+'%'};  
-            console.log(vm.gradeWidth);
-            console.log( vm.courseData);
+
+
+            loadTechnologies();
+
+
         }
-    
-    
-    
-    
+
+        function loadTechnologies(){
+            RegService.GetTechnologies()
+                .then(function (response) {
+                if (response.success) {
+                    vm.technologies = response.data.technologies;
+                } 
+            },function(response){
+                console.log("supongo1")
+            });
+        }
+
+        vm.toggleSelectionTech = function toggleSelectionTech(technology) {
+            var idx = Tecnologias.indexOf(technology.TechnologyId);
+
+            // is currently selected
+            if (idx > -1) {
+                Tecnologias.splice(idx, 1);
+            }
+
+            // is newly selected
+            else {
+                Tecnologias.push(technology.TechnologyId);
+            }
+        };
+        
+       function suggestProject(name,dateStart,dateEnd,description,additional){
+           
+           var send = {Tech}
+           console.log();
+       }
+
+
     }
 })();
