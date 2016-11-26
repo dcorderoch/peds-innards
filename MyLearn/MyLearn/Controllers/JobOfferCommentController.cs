ced3895c3082d005b46e40d993796dd0ebe044ea
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-25 18:55:20 -0600

    add job offer comment controller and methods

---
+++ MyLearn/MyLearn/Controllers/JobOfferCommentController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+using MyLearn.BLL;
+using MyLearn.Models;
+using MyLearn.InputModels;
+
+namespace MyLearn.Controllers
+{
+    public class JobOfferCommentController : ApiController
+    {
+        [HttpPost]
+        public JsonResult<ReturnCode> Create(NewJobComment jobComment)
+        {
+            var jobCmntMngr = new JobCommentManager();
+            var retVal = jobCmntMngr.CreateComment(jobComment);
+            return Json(retVal);
+        }
+        [HttpPost]
+        public JsonResult<List<JobOfferComment>> GetAll(JobOfferIdentifier jobOfferId)
+        {
+            var jobCmntMngr = new JobCommentManager();
+            var retVal = jobCmntMngr.GetAllComments(jobOfferId.JobOfferId);
+            return Json(retVal);
+        }
+    }
+}
