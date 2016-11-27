6bfe7743e9ab63459f8d29f9c800258c5f4b18c1
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-27 15:09:35 -0600

    change typo

--- MyLearn/MyLearn/InputModels/CloseJobOffer.cs
+++ MyLearn/MyLearn/InputModels/CloseJobOffer.cs
@@ -5,5 +5,6 @@
         public string JobOfferId { get; set; }
         public int State { get; set; }
         public string StateDescription { get; set; }
+        public int Stars { get; set; }
     }
 }
