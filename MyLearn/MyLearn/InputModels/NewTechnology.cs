533cfd3d7758edbc445651e2d4fd456b806e25b9
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-27 14:49:22 -0600

    remove unnecessary using directives from Models
    remove unnecessary using directives from inputmodels

--- MyLearn/MyLearn/InputModels/NewTechnology.cs
+++ MyLearn/MyLearn/InputModels/NewTechnology.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace MyLearn.InputModels
+﻿namespace MyLearn.InputModels
 {
     public class NewTechnology
     {
