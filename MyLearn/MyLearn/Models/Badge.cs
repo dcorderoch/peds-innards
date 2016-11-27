8616dec63a4298f38586fc5fcd7aab9c80e9dc4b
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-27 14:44:51 -0600

    add missing BadgeId attribute in Badges in Specific Course

--- MyLearn/MyLearn/Models/Badge.cs
+++ MyLearn/MyLearn/Models/Badge.cs
@@ -8,6 +8,7 @@ namespace MyLearn.Models
     public class Badge
     {
         public string BadgeDescription { get; set; }
+        public string BadgeId { get; set; }
         public int Value { get; set; }
         public int Awarded { get; set; }
         public int Alardeado { get; set; }
