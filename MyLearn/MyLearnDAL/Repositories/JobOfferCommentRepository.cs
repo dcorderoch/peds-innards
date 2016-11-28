62398a495311cc91db20a37ca33e8f2319aefc76
Manuel Zumbado <manu3193@gmail.com>
2016-11-28 10:32:38 -0600

    Add JobOffercomment repo

---
+++ MyLearn/MyLearnDAL/Repositories/JobOfferCommentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLearnDAL.Models;
+
+namespace MyLearnDAL.Repositories
+{
+    public class JobOfferCommentRepository: Repository<JobOfferComment>
+    {
+        public List<JobOfferComment> GetJobOfferCommentsByJobOfferId(Guid jobOfferId)
+        {
+            return DbSet.Where(joc => joc.JobOffer.JobOfferId.Equals(jobOfferId)).ToList();
+        }
+    }
+}
