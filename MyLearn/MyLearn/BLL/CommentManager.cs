5d9d67f470bef31ed56a2fea44f647b7db9d2adb
Kevin <kevgiso@hotmail.com>
2016-11-28 12:01:09 -0600

    Work on Comment manager

--- MyLearn/MyLearn/BLL/CommentManager.cs
+++ MyLearn/MyLearn/BLL/CommentManager.cs
@@ -1,15 +1,31 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using MyLearn.InputModels;
 using MyLearn.Models;
+using MyLearnDAL.Models;
+using MyLearnDAL.Repositories;
 
 namespace MyLearn.BLL
 {
     public class CommentManager
     {
         public List<Comment> GetAllComments(CommentInfo commentInfo)
-        {
+        {   
+            ProjectCommentRepository commentRepo = new ProjectCommentRepository();
+            ProjectRepository projectRepo = new ProjectRepository();
+            Project project = projectRepo.GetProjectByStudentAndCourseId(new Guid(commentInfo.StudentId), new Guid(commentInfo.CourseId));
             List<Comment> allComments = new List<Comment>();
+            List<MyLearnDAL.Models.ProjectComment> projectComments = commentRepo.GetProjectCommentByProjectId(project.ProjectId);
 
+            foreach (ProjectComment projectComment in projectComments)
+            {
+                Comment comment = new Comment();
+                comment.CommentContent = projectComment.Comment;
+                comment.CommentId = projectComment.CommentId.ToString();
+                comment.ParentId = projectComment.ParentId.ToString();
+                comment.IsFromStudent = IsStudent(projectComment.UserId);
+                
+            }
             return allComments;
 
         }
@@ -23,6 +39,37 @@ namespace MyLearn.BLL
 
             success.ReturnStatus = 1;
             return success;
+        }
+
+        private int IsStudent(Guid userId)
+        {
+         UserRepository userRepo = new UserRepository();
+            int retVal =0;
+            User user = userRepo.GetUserById(userId);
+            if (user.RoleId == 1)
+            {
+                retVal = 1;
+            }
+            return retVal;
+        }
+
+        private List<List<Comment>> ObtainNestedComments(List<MyLearnDAL.Models.ProjectComment> projectComments)
+        {
+            List<List<Comment>> resultComments = new List<List<Comment>>();
+            List<Comment> parentComments = new List<Comment>();
+
+            foreach (ProjectComment projectComment in projectComments)
+            {
+                Comment comment = new Comment();
+                if (projectComment.ParentId == Guid.Empty)
+                {
+                    comment.CommentId = projectComment.CommentId.ToString();
+                    comment.CommentContent = projectComment.Comment;
+                    
+
+                    //parentComments.Add();
+                }
+            }    
         }
     }
 }
