fef8fe3b0318efa03d2b5b7c942336307161d37f
Manuel Zumbado <manu3193@gmail.com>
2016-11-28 10:29:45 -0600

    Add Projectcomment repo

---
+++ MyLearn/MyLearnDAL/Repositories/ProjectCommentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLearnDAL.Models;
+
+namespace MyLearnDAL.Repositories
+{
+    public class ProjectCommentRepository : Repository<ProjectComment>
+    {
+        public List<ProjectComment> GetProjectCommentByProjectId(Guid projectId)
+        {
+            return DbSet.Where(pc => pc.ProjectId.Equals(projectId)).ToList();
+        }
+    }
+}
