1ea1f77db7513377c34163c5fea6463c22320f83
Kevin <kevgiso@hotmail.com>
2016-11-29 10:15:04 -0600

    Finish Bid Manager logic.

--- MyLearn/MyLearn/BLL/BidManager.cs
+++ MyLearn/MyLearn/BLL/BidManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using MyLearn.InputModels;
 using MyLearn.Models;
 using MyLearnDAL.Models;
@@ -13,22 +14,36 @@ namespace MyLearn.BLL
             var retVal = new ReturnCode();
             BidRepository bidRepo = new BidRepository();
             Bid bid = new Bid();
-
-            bid.BidId = Guid.NewGuid();
-            bid.UserId = new Guid(newBid.StudentUserId);
-            bid.JobOfferId = new Guid(newBid.JobOfferId);
-            bid.Money = newBid.Money;
-            bid.Duration = Convert.ToInt32(newBid.DurationDays);
-/*            
-        public int Duration { get; set; }*/
-
-        retVal.ReturnStatus = 1;
+            if (bid.Money <= 0)
+            {
+                retVal.ReturnStatus = 0;
+            }
+            else
+            {
+                bid.BidId = Guid.NewGuid();
+                bid.UserId = new Guid(newBid.StudentUserId);
+                bid.JobOfferId = new Guid(newBid.JobOfferId);
+                bid.Money = newBid.Money;
+                bid.Duration = Convert.ToInt32(newBid.DurationDays);
+                bidRepo.Add(bid);
+                bidRepo.SaveChanges();
+                retVal.ReturnStatus = 1;
+            }
+            bidRepo.Dispose();
             return retVal;
         }
         public ReturnCode Update(NewBid newBid)
         {
             var retVal = new ReturnCode();
-            // SUBJECT TO CHANGE
+            BidRepository bidRepo = new BidRepository();
+            List<Bid> bids = bidRepo.GetStudentBids(new Guid(newBid.StudentUserId));
+            Bid bid = bids.Find(x => x.JobOfferId.ToString()==newBid.JobOfferId);
+            if(bid != null)
+            {
+                bid.Duration = Convert.ToInt32(newBid.DurationDays);
+                bid.Money = newBid.Money;
+                retVal.ReturnStatus = 1;
+            }
             return retVal;
         }
     }
