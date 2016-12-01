c909891751b836022635238bbb869edef54bc665
Manuel Zumbado <manu3193@gmail.com>
2016-11-30 21:22:49 -0600

    Update migrations

--- MyLearn/MyLearnDAL/Migrations/201612010317174_initial.Designer.cs
+++ MyLearn/MyLearnDAL/Migrations/201612010317174_initial.Designer.cs
@@ -13,7 +13,7 @@ namespace MyLearnDAL.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201612010202342_initial"; }
+            get { return "201612010317174_initial"; }
         }
         
         string IMigrationMetadata.Source
