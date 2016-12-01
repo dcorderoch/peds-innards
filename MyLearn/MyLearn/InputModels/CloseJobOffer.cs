a29cccb9380b1d8beb8ee0fdcc0cbec6bcafb78d
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-30 19:37:33 -0600

    fix wrong types in close job offer

--- MyLearn/MyLearn/InputModels/CloseJobOffer.cs
+++ MyLearn/MyLearn/InputModels/CloseJobOffer.cs
@@ -3,8 +3,8 @@
     public class CloseJobOffer
     {
         public string JobOfferId { get; set; }
-        public float State { get; set; }
+        public int State { get; set; }
         public string StateDescription { get; set; }
-        public float Stars { get; set; }
+        public int Stars { get; set; }
     }
 }
