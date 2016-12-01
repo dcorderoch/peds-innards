b4b63e9c1281bccb5a388e8a13d53e3748a8ce01
EL David Cordero Chavarría <dacoch215@estudiantec.cr>
2016-11-30 22:24:16 -0600

    fix error in comment info model

--- MyLearn/MyLearn/InputModels/CommentInfo.cs
+++ MyLearn/MyLearn/InputModels/CommentInfo.cs
@@ -2,7 +2,7 @@
 {
     public class CommentInfo
     {
-        public string StudentId { get; set; }
+        public string StudentUserId { get; set; }
         public string ProfessorId { get; set; }
         public string CourseId { get; set; }
     }
