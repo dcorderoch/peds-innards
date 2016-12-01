2c55d091cc900c418d30cc1f8c786f62da90d212
Kevin <kevgiso@hotmail.com>
2016-12-01 13:50:43 -0600

    Work on Job Comment manager.

--- MyLearn/MyLearn/BLL/JobCommentManager.cs
+++ MyLearn/MyLearn/BLL/JobCommentManager.cs
@@ -1,17 +1,41 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using MyLearn.InputModels;
 using MyLearn.Models;
+using MyLearnDAL;
+using MyLearnDAL.Models;
+using MyLearnDAL.Repositories;
+using JobOfferComment = MyLearn.Models.JobOfferComment;
 
 namespace MyLearn.BLL
 {
     public class JobCommentManager
     {
         public List<JobOfferComment> GetAllComments(string jobOfferId)
-        {
-            List<JobOfferComment> allComments = new List<JobOfferComment>();
+        {/*
+            using (var context = new MyLearnContext())
+            {
+                ProjectCommentRepository commentRepo = new ProjectCommentRepository(context);
+                ProjectRepository projectRepo = new ProjectRepository(context);
+                Project project = projectRepo.GetProjectByStudentAndCourseId(new Guid(commentInfo.StudentUserId), new Guid(commentInfo.CourseId));
+                List<Comment> allComments = new List<Comment>();
+                List<MyLearnDAL.Models.ProjectComment> projectComments = (project == null) ? new List<MyLearnDAL.Models.ProjectComment>() : commentRepo.GetProjectCommentByProjectId(project.ProjectId);
+                List<List<Comment>> splitComments = ObtainNestedComments(projectComments);
+                List<Comment> parentComments = splitComments[0];
+                List<Comment> childComments = splitComments[1];
 
-            return allComments;
+                foreach (Comment childComment in childComments)
+                {
+                    Comment parentComment = parentComments.Find(x => x.CommentId == childComment.ParentId);
+                    parentComment.NestedComments.Add(childComment);
+                    allComments.Add(parentComment);
+                }
 
+                commentRepo.Dispose();
+                projectRepo.Dispose();
+                return allComments;
+            }*/
+            return new List<JobOfferComment>();
         }
 
         public ReturnCode CreateComment(NewJobComment newJobComment)
