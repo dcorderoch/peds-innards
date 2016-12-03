febeae2b081df8b78ca1cf19fcd588c32807db4b
Kevin <kevgiso@hotmail.com>
2016-12-03 04:02:53 -0600

    Comment university manager.

--- MyLearn/MyLearn/BLL/UniversityManager.cs
+++ MyLearn/MyLearn/BLL/UniversityManager.cs
@@ -5,8 +5,15 @@ using MyLearnDAL.Repositories;
 
 namespace MyLearn.BLL
 {
+    /// <summary>
+    /// Class built in order to manage universities in MyLearn.
+    /// </summary>
     public class UniversityManager
     {
+        /// <summary>
+        /// Obtains a list of universities that are present in the platform.
+        /// </summary>
+        /// <returns>List of all universities.</returns>
         public List<University> GetAllUniversities()
         {
             using (var context = new MyLearnContext())
