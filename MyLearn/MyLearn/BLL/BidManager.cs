b5f4f80a35f4c5bb642d2f5cfd74bcb4983d47a3
Manuel Zumbado <manu3193@gmail.com>
2016-12-02 18:16:46 -0600

    move object initialization into if statement

--- MyLearn/MyLearn/BLL/BidManager.cs
+++ MyLearn/MyLearn/BLL/BidManager.cs
@@ -15,19 +15,21 @@ namespace MyLearn.BLL
             using (var context = new MyLearnContext())
             {
                 var retVal = new ReturnCode();
-                BidRepository bidRepo = new BidRepository(context);
-                Bid bid = new Bid();
+                var bidRepo = new BidRepository(context);
                 if (newBid.Money <= 0)
                 {
                     retVal.ReturnStatus = 0;
                 }
                 else
                 {
-                    bid.BidId = Guid.NewGuid();
-                    bid.UserId = new Guid(newBid.StudentUserId);
-                    bid.JobOfferId = new Guid(newBid.JobOfferId);
-                    bid.Money = newBid.Money;
-                    bid.Duration = Convert.ToInt32(newBid.DurationDays);
+                    var bid = new Bid
+                    {
+                        BidId = Guid.NewGuid(),
+                        UserId = new Guid(newBid.StudentUserId),
+                        JobOfferId = new Guid(newBid.JobOfferId),
+                        Money = newBid.Money,
+                        Duration = Convert.ToInt32(newBid.DurationDays)
+                    };
                     bidRepo.Add(bid);
                     bidRepo.SaveChanges();
                     retVal.ReturnStatus = 1;
